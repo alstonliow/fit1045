b52247f9fd06a94141e654752e37a1392355ff0b
alstonliow <alston.liow@gmail.com>
2026-08-23 10:33:55 +0800

    class

---
+++ FieldGuide/8_MemberFunc/8_account.cpp
@@ -0,0 +1,15 @@
+#include "splashkit.h"
+#include "utilities.h"
+
+class account
+{
+    // private members here...
+
+    public:
+    // public members here...
+};
+
+int main()
+{
+    return 0;
+}
