c69ca68f1c18cdfaf4ee3d820b07644c86f007e3
alstonliow <alston.liow@gmail.com>
2026-08-23 13:29:50 +0800

    user class finish with method

--- FieldGuide/8_MemberFunc/8_user.cpp
+++ FieldGuide/8_MemberFunc/8_user.cpp
@@ -6,20 +6,56 @@ class user
     string password{};
 
 public:
-    string name{};
+    string username{};
 
     // intialize account with param
-    user(string name_, string password_)
+    user(string username_, string password_)
     {
-        name = name_;
+        username = username_;
         password = password_;
     }
 
     // initialize account without param
     user()
     {
-        name = "No name";
+        username = "No name";
         password = "";
+    }
+
+    // test login, login success when name and password is correct
+    bool test_login(string username_, string password_)
+    {
+        return (username == username_) && (password == password_);
+    }
+
+    // change password
+    // old password must be correct
+    // old and new password cannot be same
+    // new password again must correct
+    bool change_password(string old_password_, string new_password_)
+    {
+        // when current password is wrong
+        if (old_password_ != password)
+        {
+            write_line("Failed to change password - invalid password");
+            return false;
+        }
+        // password reuse
+        else if (old_password_ == new_password_)
+        {
+            write_line("Failed to change password - password reuse");
+            return false;
+        }
+        // password changed
+        else if (old_password_ == password && old_password_ != new_password_)
+        {
+            password = new_password_;
+            write_line("Password changed");
+            return true;
+        }
+
+        // backup return
+        return old_password_ == password && old_password_ != new_password_;
     }
 };
 
