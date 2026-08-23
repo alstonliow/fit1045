0f179753506cefc87ba60fa78f385bd079f43448
alstonliow <alston.liow@gmail.com>
2026-08-23 11:04:01 +0800

    constructor

--- FieldGuide/8_MemberFunc/8_account.cpp
+++ FieldGuide/8_MemberFunc/8_account.cpp
@@ -3,10 +3,33 @@
 
 class account
 {
-    // private members here...
+    // The balance of account in cent
+    int balance{};
 
-    public:
-    // public members here...
+public:
+    // The name of account holder
+    string name{};
+
+    // counstructor with param
+    account(string name_, int balance_)
+    {
+        // initialize the account
+        name = name_;
+        balance = balance_;
+    }
+
+    // constructor without param
+    account()
+    {
+        name = "Account holder unknown";
+        balance = 0;
+    }
+
+    // print method
+    void print()
+    {
+        write_line(name + ": $" + to_string(balance / 100.0));
+    }
 };
 
 int main()
