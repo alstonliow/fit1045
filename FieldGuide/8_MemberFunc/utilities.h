53f12aa0eaa978ca3a0dcfbdde0311811ee2aaf2
alstonliow <alston.liow@gmail.com>
2026-08-23 11:46:13 +0800

    write comment

--- FieldGuide/8_MemberFunc/utilities.h
+++ FieldGuide/8_MemberFunc/utilities.h
@@ -34,7 +34,21 @@ int read_integer(string prompt);
 
 int read_integer(string prompt, int low, int high);
 
+/**
+ * Read a floating point number from user
+ *
+ * @param prompt message
+ * @returns the number entered
+ */
+
 double read_double(string prompt);
+
+/**
+ * Check if a value is less than zero
+ *
+ * @param value the value to test
+ * @returns true when value is below zero
+ */
 
 template <typename T>
 bool is_negative(T value)
@@ -42,7 +56,21 @@ bool is_negative(T value)
     return value < 0;
 }
 
+/**
+ * Read a floating point number from user, rejecting negatives
+ *
+ * @param prompt message
+ * @returns a number that is zero or above
+ */
+
 double read_non_negative_double(string prompt);
+
+/**
+ * Read an integer from user, rejecting negatives
+ *
+ * @param prompt message
+ * @returns an integer that is zero or above
+ */
 
 int read_non_negative_int(string prompt);
 
